3ccae55c59e6759e25582e01fd6087d5c4c8767b
munmo <sunahjung7@gmail.com>
2023-08-21 11:10:41 +1000

    profile  update

---
+++ server/server.js
@@ -0,0 +1,17 @@
+var express = require('express'); //Used for routing 
+var app = express();
+var http = require('http').Server(app); //Used to provide http functionality 
+var cors = require('cors');
+var path = require('path');
+app.use(cors());
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+let server = http.listen(3000, function() {
+    let host = server.address().address;
+    let port = server.address().port;
+    console.log("Server listening on port: " + port);
+});
+
+app.post('/api/auth', require('./routes/login'));
